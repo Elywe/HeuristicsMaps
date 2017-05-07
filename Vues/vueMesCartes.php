16c947214beb6a8cba227a29655e4c6c1b116235
Mélina <melina.capron1@gmail.com>
2017-05-07 16:11:18 +0200

    affichage des boutons des noeuds

--- Vues/vueMesCartes.php
+++ Vues/vueMesCartes.php
@@ -15,40 +15,44 @@
     }
     if (isset($_SESSION['pseudo'])) {
         ?>
-
-        <form action="index.php?section=mesCartes&action=ajoutCarte" method="post">
-            <div>
-                <h2>Ajouter une carte</h2>
-                <input class="ajoutBox"  type="text" name="nom" id="nom" required/>
-                <select id="visibilite" name="visibilite">
-                    <option value="public">Public</option>
-                    <option value="prive">Privé</option>
-                </select>
-                <input type="submit" class="bouton" id="Valider" name="Valider" value="Valider">
-            </div>
-        </form>
+        <div class="ajouterCarte">
+            <form action="index.php?section=mesCartes&action=ajoutCarte" method="post">
+                <div>
+                    <h2>Ajouter une carte</h2>
+                    <input class="ajoutBox"  type="text" name="nom" id="nom" required/>
+                    <select id="visibilite" name="visibilite">
+                        <option value="public">Public</option>
+                        <option value="prive">Privé</option>
+                    </select>
+                    <input type="submit" class="bouton" id="Valider" name="Valider" value="Valider">
+                </div>
+            </form>
+        </div>
         <?php
     }
     ?>
-    <h2>Visualiser mes cartes</h2>
-    <form class="carteBox" method="post" action="index.php?section=mesCartes">
-        <select id="carte" name="carte">
-            <?php
-            if (!isset($idCarte)) {
-                $idCarte = isset($_POST['carte']) ? $_POST['carte'] : -1;
-            }
-            foreach ($cartes as $carte) {
-                echo "<option value=\"" . $carte->getIdentifiant() . "\"" . ($idCarte == $carte->getIdentifiant() ? " selected" : "") . ">" . $carte->getNom() . "</option>";
-            }
-            ?>
-        </select>
-        <div class="boutonsCartes">
-            <input type="submit" class="bouton" name="Valider" value="Valider">
-            </form>
-            <a class="bouton" id="Supprimer">Supprimer</a>
-            <a class="bouton" id="Renommer">Renommer</a>
-            <a class="bouton" id="Partager">Partager</a>
-        </div>
+    <div class=visuCartes>
+        <h2>Visualiser mes cartes</h2>
+        <form class="carteBox" method="post" action="index.php?section=mesCartes">
+            <select id="carte" name="carte">
+                <?php
+                if (!isset($idCarte)) {
+                    $idCarte = isset($_POST['carte']) ? $_POST['carte'] : -1;
+                }
+                foreach ($cartes as $carte) {
+                    echo "<option value=\"" . $carte->getIdentifiant() . "\"" . ($idCarte == $carte->getIdentifiant() ? " selected" : "") . ">" . $carte->getNom() . "</option>";
+                }
+                ?>
+            </select>
+            <div class="boutonsCartes">
+                <input type="submit" class="bouton" name="Valider" value="Valider">
+                </form>
+                <a class="bouton" id="Supprimer">Supprimer</a>
+                <a class="bouton" id="Renommer">Renommer</a>
+                <a class="bouton" id="Partager">Partager</a>
+            </div>
+    </div>
+    <div class="affichageNoeuds">
         <table>
             <?php
             if (isset($noeuds)) {
@@ -62,4 +66,5 @@
             }
             ?>
         </table>
+    </div>
 </div>
