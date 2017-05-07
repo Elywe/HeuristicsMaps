625c2ec78e3faf8bf2b7ebcaadaa44021d5410ab
Mélina <melina.capron1@gmail.com>
2017-05-07 09:35:12 +0200

    debug partage

--- Vues/vueMesCartes.php
+++ Vues/vueMesCartes.php
@@ -4,7 +4,7 @@
         ?>
         <div class = "alert">
             <span class = "closebtn" onclick = "this.parentElement.style.display = 'none';">&times;</span>
-            <strong>Partage :</strong> Lien de la carte partagée : <input class = "ajoutBox" type = "text" name = "partage" id = "partage" value = "http://localhost/ProjetWeb/index.php?section=partage&numCarte=<?php echo $carte->getIdentifiant() ?>"/>
+            <strong>Partage :</strong> Lien de la carte partagée : <input class = "ajoutBox" type = "text" name = "partage" id = "partage" value = "http://localhost/ProjetWeb/index.php?section=mesCartes&numCarte=<?php echo $carte->getIdentifiant() ?>"/>
         </div>
         <?php
     }
